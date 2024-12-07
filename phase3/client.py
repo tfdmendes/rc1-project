61b3bce1460cef81b0669fafc0dfda46447b595c
André Vasconcelos <vasconcelos13@ua.pt>
2024-12-07 00:02:19 +0000

    Server e Client Terminados?

--- phase3/client.py
+++ phase3/client.py
@@ -24,12 +24,27 @@ ip_addr = sys.argv[1]
 udp_port = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.settimeout(1)
+sock.settimeout(2)
+
+try:
+    sock.sendto("ping".encode(), (ip_addr, udp_port))
+    data, addr = sock.recvfrom(2048)
+    if data.decode() == "pong":
+        print("Conexão com o servidor bem-sucedida!")
+except socket.timeout:
+    print("Não foi possível conectar ao servidor")
+    sys.exit(1)
 
 while True:
     try:
+        try:
+            data, addr = sock.recvfrom(2048)
+            print(data.decode())
+        except socket.timeout:
+            print("Nenhuma resposta do servidor.")
+
         message = input("Mensagem para enviar? ").encode()
-        if (message == "exit".encode() or message == "sair".encode()):
+        if message == "exit".encode() or message == "sair".encode():
             message = "A sair"
             for i in range(4):
                 sys.stdout.write("\r" + message + "." * i)
@@ -40,11 +55,6 @@ while True:
         if len(message) > 0:
             sock.sendto(message, (ip_addr, udp_port))
 
-        try:
-            data, addr = sock.recvfrom(2048)
-            print(data.decode())
-        except socket.timeout:
-            print("Nenhuma resposta do servidor.")
 
     except KeyboardInterrupt:
         break
--- phase3/server.py
+++ phase3/server.py
@@ -18,20 +18,56 @@ sock.bind((ip_addr, udp_port))
 
 print(f"Servidor UDP iniciado em {ip_addr}:{udp_port}")
 
+client_names = {}
 client_games = {}
+num_games = 0;
+all_games = {}
 
 while True:
     msg, sender_addr_port = sock.recvfrom(2048)
     msg_decoded = msg.decode().strip()
     print('Mensagem de {}:{} -> {}'.format(sender_addr_port[0], sender_addr_port[1], msg_decoded))
 
-    if msg_decoded == "game":
+    if msg_decoded == "ping":
+        sock.sendto("pong".encode(), sender_addr_port)
+        if sender_addr_port not in client_names:
+            sock.sendto("Por favor, introduz um username ↓".encode(), sender_addr_port)
+            name_msg, _ = sock.recvfrom(2048)
+            name = name_msg.decode().strip()
+            client_names[sender_addr_port] = name
+            sock.sendto(f"Bem-vindo {name}!".encode(), sender_addr_port)
+            print(f"Cliente {sender_addr_port} registado como {name}")
+        continue
+
+    elif msg_decoded == "record":
+        all_games = dict(sorted(all_games.items(), key=lambda item: item[1]['attempts']))
+        tabela = "+-----------------+-----------------+-------+-----------+\n"
+        tabela += "| Username        | IP              | Port  | Attempts  |\n"
+        tabela += "+-----------------+-----------------+-------+-----------+\n"
+        
+        for game in all_games.values():
+            tabela += "| {name:<15} | {ip:<15} | {port:<5} | {attempts:<9} |\n".format(
+                name=game['name'],
+                ip=game['sender_addr_port'][0],
+                port=game['sender_addr_port'][1],
+                attempts=game['attempts']
+            )
+        
+        tabela += "+-----------------+-----------------+-------+-----------+"
+        
+        # Enviar a tabela formatada de volta para o cliente
+        sock.sendto(tabela.encode(), sender_addr_port)
+
+    elif msg_decoded == "game":
         if sender_addr_port not in client_games:
             server_choice = random.randint(1, 100)
+
             client_games[sender_addr_port] = {
+                "name": client_names[sender_addr_port],
                 "server_choice": server_choice,
                 "attempts": 0
             }
+
             print(f"Novo jogo para o cliente {sender_addr_port}")
             print(f"Número gerado: {server_choice}")
 
@@ -44,14 +80,28 @@ while True:
             client_choice = int(msg_decoded)
 
             if client_choice > game["server_choice"]:
-                msg = "Valor muito alto, tenta um mais baixo!".encode()
+                if client_choice - game["server_choice"] <= 10:
+                    msg = "Valor alto, tenta um mais baixo!".encode()
+                else:
+                    msg = "Valor muito alto, tenta um mais baixo!".encode()
                 game["attempts"] += 1
             elif client_choice < game["server_choice"]:
-                msg = "Valor muito baixo, tenta um mais alto!".encode()
+                if game["server_choice"] - client_choice <= 10:
+                    msg = "Valor baixo, tenta um mais alto!".encode()
+                else:
+                    msg = "Valor muito baixo, tenta um mais alto!".encode()
                 game["attempts"] += 1
             else:
                 game["attempts"] += 1
-                msg = f"Parabéns! Conseguiste acertar, com {game['attempts']} tentativa(s).\nO valor era: {game['server_choice']}.".encode()
+                msg = f"Parabéns {game["name"]}! Conseguiste acertar, com {game['attempts']} tentativa(s).\nO valor era: {game['server_choice']}.".encode()
+
+                all_games[num_games] = {
+                    "name": client_names[sender_addr_port],
+                    "sender_addr_port": sender_addr_port,
+                    "attempts": game["attempts"]
+                }
+                num_games += 1;
+
                 del client_games[sender_addr_port]
 
         except ValueError:
